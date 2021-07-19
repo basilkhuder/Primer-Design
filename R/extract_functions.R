217269e46cde9181a0f95d550faf87f8989d641f
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2021-07-19 13:41:37 -0700

    Update extract_functions.R

--- R/extract_functions.R
+++ R/extract_functions.R
@@ -17,7 +17,7 @@ primer_extract <- function(doc) {
   string_test <- stringr::str_subset(primer_df, "For_and")
   
   if (length(string_test) == 0) {
-    string_test <- str_subset(primer_df, "_and_")
+    string_test <- stringr::str_subset(primer_df, "_and_")
     gene <- stringr::str_match(string_test, "(.*)Ex.*_and_")[[2]]
     
   } else {
