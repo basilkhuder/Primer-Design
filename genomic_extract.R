afc46bc11f0cfef50ccd61e094fdca19939083e5
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2021-07-19 09:20:52 -0700

    Update genomic_extract.R

--- genomic_extract.R
+++ genomic_extract.R
@@ -1,8 +1,8 @@
 genomic_extract <- function(doc) {
-  
-  main_df <- paragraph_extract(doc)
+  main_df <- paragraph_extract(doc)[1]
   gene_name <- stringr::str_match(main_df, "(.*)Exon")[[2]]
-  position <- stringr::str_match(main_df, "Genomic Coordinates: (.*)V")[[2]]
+  position <-
+    stringr::str_match(main_df, "Genomic Coordinates: (.*)V")[[2]]
   exon <- stringr::str_extract(main_df, "Exon \\d*")
   main_df <- data.frame(Gene = gene_name,
                         "Genomic_Position" = position,
@@ -15,8 +15,8 @@ genomic_extract <- function(doc) {
 paragraph_extract <- function(doc) {
   doc <- officer::read_docx(doc)
   doc <- officer::docx_summary(doc)
-  doc <- doc[doc["content_type"] == "paragraph", ]
-  doc <- doc[["text"]]
+  doc <- doc[doc["content_type"] == "paragraph",]
+  doc <- doc[["text"]][1]
   return(doc)
   
 }
